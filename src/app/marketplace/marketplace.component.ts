831fd9dd62e050a37ee2cccd55587af74be9179d
bh <bwhellar@gmail.com>
2019-06-13 20:34:47 -0700

    this will push the details

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,23 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
+import { AlbumService} from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
-export class MarketplaceComponent {
+export class MarketplaceComponent implements OnInit {
 
-  constructor(private router: Router){}
+  albums: Album[];
 
-  albums: Album[] = [
-      new Album("The Night Is Young", "By: The 2 Bears",
-      "Raf Rundell and Joe Goddard return with more four-to-the-floor fun, this time seemingly aimed at thirtysomethings coming to terms with their lost youth", 1),
-      new Album("The Lions Roar", "By: First Aid Kit", " With its soaring flutes and layered harmonies, \"The Lion\'s Roar\" conjures the pastoral, almost Andean sound of Fleet Foxes\' \"Your Protector.\" Stomping drums and a strummed acoustic guitar bring welcome fire to the mix. However, it\'s the Söderberg sisters\' soulful vocal harmonies — especially during a short but haunting a cappella interlude — that set First Aid Kit apart.", 2),
-      new Album ("Since I Left You", "By: The Avalanches", "Much of the beauty of the opening title song and its accompanying track, \"Stay Another Season,\" lies in the way that the Avalanches turn obvious sonic mismatches into something all their own. It\'s not too common that you\'ll hear a sample of a horse, a rastafarian singer, and an invitation to a Club Med disco all in the same song, but somehow it makes perfect sense under the masterful direction of the Avalanches.", 3),
-      new Album ("Grey Veils", "By: Chainless", "Symphonic compositions coupled with plunderphonic-sample styled beats make for an intensely brooding vibe that'll either stir you up or shake you down depending on your disposition.", 4)
-  ];
+  constructor(private router: Router, private albumService: AlbumService){}
+
+  ngOnInit()  {
+    this.albums = this.albumService.getAlbums();
+  }
 
   goToDetailPage(clickedAlbum: Album) {
     this.router.navigate(['albums', clickedAlbum.id]);
---
+++ src/app/mock-albums.ts
@@ -0,0 +1,9 @@
+import { Album } from './album.model';
+
+export const ALBUMS: Album[] = [
+  new Album("The Night Is Young", "By: The 2 Bears",
+  "Raf Rundell and Joe Goddard return with more four-to-the-floor fun, this time seemingly aimed at thirtysomethings coming to terms with their lost youth", 1),
+  new Album("The Lions Roar", "By: First Aid Kit", " With its soaring flutes and layered harmonies, \"The Lion\'s Roar\" conjures the pastoral, almost Andean sound of Fleet Foxes\' \"Your Protector.\" Stomping drums and a strummed acoustic guitar bring welcome fire to the mix. However, it\'s the Söderberg sisters\' soulful vocal harmonies — especially during a short but haunting a cappella interlude — that set First Aid Kit apart.", 2),
+  new Album ("Since I Left You", "By: The Avalanches", "Much of the beauty of the opening title song and its accompanying track, \"Stay Another Season,\" lies in the way that the Avalanches turn obvious sonic mismatches into something all their own. It\'s not too common that you\'ll hear a sample of a horse, a rastafarian singer, and an invitation to a Club Med disco all in the same song, but somehow it makes perfect sense under the masterful direction of the Avalanches.", 3),
+  new Album ("Grey Veils", "By: Chainless", "Symphonic compositions coupled with plunderphonic-sample styled beats make for an intensely brooding vibe that'll either stir you up or shake you down depending on your disposition.", 4)
+];
